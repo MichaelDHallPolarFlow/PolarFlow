6d283aed1b7ca6a096db5a83e21bbbb4b1643c2a
MichaelDHallPolarFlow <129086783+MichaelDHallPolarFlow@users.noreply.github.com>
2023-04-25 11:40:31 -0500

    Update heartratemin.sh

--- bash/heartratemin.sh
+++ bash/heartratemin.sh
@@ -1,1 +1,5 @@
+#This rule is made for when a user specifies ONE Training Activity in bash/jq.sh bash script. i.e. "POOL_SWIMMING"
 jq '.heartRate.min' $1
+
+#This rule is made for when a user does not specify a Training Activity, and therefore ALL Training Activities are in output files
+jq '.exercises[].heartRate.min' $1
