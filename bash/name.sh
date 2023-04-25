ddb1d02b1e4ff3048e29917575260c67c8cd13be
MichaelDHallPolarFlow <129086783+MichaelDHallPolarFlow@users.noreply.github.com>
2023-04-25 11:43:49 -0500

    Update name.sh

--- bash/name.sh
+++ bash/name.sh
@@ -1,1 +1,5 @@
+#This rule is made for when a user specifies a UNIQUE Training Activity in bash/jq.sh bash script
 jq '.sport' $1
+
+#This rule is made for when a user does not specify a UNIQUE Training Activity, and therefore, by default, ALL Training Activity is made in output files
+jq '.exercises[].sport' $1
