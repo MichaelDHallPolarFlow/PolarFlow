e4a9f091974e4070af653db7e20bd88d12f693df
MichaelDHallPolarFlow <129086783+MichaelDHallPolarFlow@users.noreply.github.com>
2023-04-25 11:42:15 -0500

    Update jq.sh

--- bash/jq.sh
+++ bash/jq.sh
@@ -1,1 +1,5 @@
+#This rule is made for when a user would like to filter their JSON Training Data by a specific sport like Strength Training 
 jq '.exercises[] | select(.sport == "STRENGTH_TRAINING")' Training/*.json
+
+#This rule is made for when a user does not specify a Unique Training Activity, and therefore, by default ALL Training Activities are made in output files
+jq '.' Training/*.json
