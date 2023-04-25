ddafb6fd2d88ad60a616d297898b5033ef20f3d4
MichaelDHallPolarFlow <129086783+MichaelDHallPolarFlow@users.noreply.github.com>
2023-04-25 11:36:57 -0500

    Update heartrateavg.sh

--- bash/heartrateavg.sh
+++ bash/heartrateavg.sh
@@ -1,1 +1,6 @@
+#Please comment at least one of these rules out
+#This rule is made for when user specifies ONE Training Activity in bash/jq.sh bash script i.e. "POOL_SWIMMING"
 jq '.heartRate.avg' $1
+
+#This rule is made for when user does not specify a Training Activity, and therefore, ALL Training Activity is made in output files
+jq '.exercises[].heartRate.avg' $1
